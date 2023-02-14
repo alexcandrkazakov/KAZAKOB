58aaef96d3059a54212fb448480d8d6626f3b88f
alexcandrkazakov <124293258+alexcandrkazakov@users.noreply.github.com>
2023-02-14 11:40:46 +0300

    14.02.2023 практическая 2

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-my_list=[True, 786, 3.14, 'text', 70.2]
-second_list=[123, 'text']
-print(my_list)
-print(my_list[0])
-print(my_list[1:3])
-print(my_list[2:])
-print(second_list*2)
-print(my_list + second_list)
-my_turle=(True, 786, 3.14, 'text', 70.2)
-second_tuple=(123, 'text')
-print(my_turle)
-print(my_turle[0])
-print(second_tuple*2)
-print(my_turle + second_tuple)
-my_dict = {}
-my_dict["contry"]="Mexico"
-print(my_dict["contry"])
-another_dict={"number":23, 2:True, "my_list":[1,2,3]}
-print(another_dict.keys())
-print(another_dict.values())
-s= set ()
-s={"hi","bye"}
+f=4
+g=10
+print((f!=g)and (g>f))
+print((f==g)and(f>g))
+print((f!=g)or (g>f))
+print((f==g)or (f>g))
+a="stop"
+v="vid"
+print((a>v) or (a!=v))
+print((a<v) and (a!=v))
+print((a>v) and (a!=v))
+
